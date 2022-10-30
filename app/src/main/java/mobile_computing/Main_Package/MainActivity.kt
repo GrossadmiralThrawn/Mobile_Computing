4ac734cd892de20510d0dc927fe1e799029e2ccd
GrossadmiralThrawn <lukasd2000@gmx.de>
2022-10-30 20:40:18 +0100

    Korrektur von Fehlern und Ergänzung einer Deutsch-XML Datei

--- app/src/main/java/mobile_computing/Main_Package/MainActivity.kt
+++ app/src/main/java/mobile_computing/Main_Package/MainActivity.kt
@@ -2,9 +2,11 @@ package mobile_computing.Main_Package
 
 import android.content.Intent
 import android.content.SharedPreferences
+import android.content.res.Configuration
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
 import android.widget.TextView
+import java.util.*
 
 class MainActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
@@ -13,17 +15,24 @@ class MainActivity : AppCompatActivity() {
 
 
 
-        val Select_Language = Intent(this, Select_Language_Activitiy::class.java)
-        val Primary_Settings = getSharedPreferences("My_Primary_Preferences", 0)
-        var Edit_Language: SharedPreferences.Editor = Primary_Settings.edit()
-        val Language_Key = "Language Key"
+        val Select_Language                            = Intent(this, Select_Language_Activitiy::class.java)
+        val Primary_Settings                           = getSharedPreferences("My_Primary_Preferences", 0)
+        var Edit_Language:    SharedPreferences.Editor = Primary_Settings.edit()
+        val Language_Key                               = "Language Key"
+        var Settings_Setter:  Configuration            = Configuration()
+
+
 
 
 
         if ((Primary_Settings.getString(Language_Key, "") == null) || (Primary_Settings.getString(Language_Key, "") == ""))
         {
+            val Test_TextView: TextView = findViewById(R.id.TextView_MainActivity)
             var Sleeper_1: Sleeper_Class = Sleeper_Class(1000)
 
+
+
+            Test_TextView.text = "Welcome"
 
 
             Sleeper_1.start()
@@ -34,7 +43,28 @@ class MainActivity : AppCompatActivity() {
         }
         else
         {
+            val Language:      String = Primary_Settings.getString(Language_Key, "Fehler")!!
+            var Place:         Locale = Locale(Language)
+            var config:        Configuration = Configuration()
             val Test_TextView: TextView = findViewById(R.id.TextView_MainActivity)
+
+
+
+            Locale.setDefault(Place)
+
+
+            Settings_Setter.setLocale(Place)
+
+
+            config.locale = Place
+
+
+
+            baseContext.getResources().updateConfiguration(Settings_Setter, baseContext.getResources().displayMetrics)
+
+
+
+            Test_TextView.text
         }
     }
 }
