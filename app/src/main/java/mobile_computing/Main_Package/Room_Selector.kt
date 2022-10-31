81a1259d259f4db25f272ae164b5ec1e0b87ad15
GrossadmiralThrawn <108291842+GrossadmiralThrawn@users.noreply.github.com>
2022-10-31 20:11:46 +0100

    Finish.

--- app/src/main/java/mobile_computing/Main_Package/Room_Selector.kt
+++ app/src/main/java/mobile_computing/Main_Package/Room_Selector.kt
@@ -3,6 +3,7 @@ package mobile_computing.Main_Package
 import android.content.Intent
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.widget.TextView
 
 class Room_Selector : AppCompatActivity()
 {
@@ -24,10 +25,15 @@ class Room_Selector : AppCompatActivity()
 
     fun Call_Way_Descriptor_Actibvty()
     {
-        var New_Activity = Intent(this, Way_Descriptor::class.java)
+        var New_Activity                      = Intent(this, Way_Descriptor::class.java)
+        var Way_Descriptor_TextView: TextView = findViewById(R.id.Test_Textview_Way_Descriptor)
 
 
 
         New_Activity.putExtra("Room_Name", Room)
+
+
+
+        Way_Descriptor_TextView.text = Room
     }
 }
