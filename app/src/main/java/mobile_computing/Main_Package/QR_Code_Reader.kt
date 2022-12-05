c336ec0da3410959afa2c179871f8d9b8ebcab15
GrossadmiralThrawn <108291842+GrossadmiralThrawn@users.noreply.github.com>
2022-12-05 17:51:26 +0100

    Start der QR-Code

---
+++ app/src/main/java/mobile_computing/Main_Package/QR_Code_Reader.kt
@@ -0,0 +1,11 @@
+package mobile_computing.Main_Package
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+
+class QR_Code_Reader : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_qr_code_reader)
+    }
+}
