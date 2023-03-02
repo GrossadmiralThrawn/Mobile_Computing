88ec64086a6a411fb2d2e6356ac75a5bc5e8f838
GrossadmiralThrawn <lukasd2000@gmx.de>
2023-03-02 13:25:23 +0100

    Fixung des Eingabebugs und Überprüfung der Übergabe.

--- app/src/main/java/mobile_computing/Main_Package/Room_Selector.kt
+++ app/src/main/java/mobile_computing/Main_Package/Room_Selector.kt
@@ -1,5 +1,8 @@
 package mobile_computing.Main_Package
 
+
+
+
 import android.content.Intent
 import android.os.Bundle
 import android.view.View
@@ -7,6 +10,8 @@ import android.widget.Button
 import android.widget.EditText
 import android.widget.TextView
 import androidx.appcompat.app.AppCompatActivity
+
+
 
 
 class Room_Selector : AppCompatActivity() {
